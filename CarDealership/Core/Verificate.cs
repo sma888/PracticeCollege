32d96f458c778c545c8a05d3314367a5c96ef919
sma888 <magamet.sitimov@gmail.com>
2023-03-21 11:40:19 +0300

    first

---
+++ CarDealership/Core/Verificate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CarDealership.Core
+{
+    public class Verificate
+    {
+
+        public bool Check(string login, string password)
+        {
+            using (var db = new CarDealershipEntities())
+            {
+                var result = db.User.FirstOrDefault(user => user.Login == login && user.Password == password);
+
+                if (result != null)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
+
---
+++ CarDealership/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using CarDealership.Core;
+using System.Windows;
+
+namespace CarDealership.ViewModel
+{
+    class MainWindowViewModel : BaseViewModel
+    {
+        private string _password;
+        private string _login;
+
+        public string Login
+        {
+            get => _login;
+            set
+            {
+                _login = value;
+                OnPropertyChanged(nameof(Login));
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        public void Checking()
+        {
+            var verif = new Verificate();
+            bool res = verif.Check(Login, Password);
+            if (res)
+            {
+                MessageBox.Show("Авторизация прошла успешно", "Сообщение", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else MessageBox.Show("Неверный логин или пароль", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Stop);
+        }
+    }
+}
+
